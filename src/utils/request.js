9fd157df6fc86c87c8be72cbe10037834a7abb43
zzzmx <2774158143@qq.com>
2026-03-31 21:00:36 +0800

    axios的基础封装

---
+++ src/utils/request.js
@@ -0,0 +1,23 @@
+
+import axios from 'axios'
+
+
+const instance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 100000
+})
+
+//拦截器
+
+// axios请求拦截器
+instance.interceptors.request.use(config => {
+  return config
+}, e => Promise.reject(e))
+
+// axios响应式拦截器
+instance.interceptors.response.use(res => res.data, e => {
+
+  return Promise.reject(e)
+})
+
+export default instance
