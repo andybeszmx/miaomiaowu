f473ae25c6fab93aab9df8cb993922d6d71f4402
zzzmx <2774158143@qq.com>
2026-04-01 19:42:12 +0800

    懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
+import { lazyPlugin } from './directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
